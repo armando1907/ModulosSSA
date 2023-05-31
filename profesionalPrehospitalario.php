02c3d7e3e52f1ebb8ddcfca68909fd44ed31f11e
armando1907 <armando_go19@outlook.com>
2023-05-30 20:27:41 -0700

    Se modifico el navbar de profesionalPrehospitalario

--- profesionalPrehospitalario.php
+++ profesionalPrehospitalario.php
@@ -109,16 +109,14 @@ if($_POST){
                                 </a>
                             <div class="sb-sidenav-menu-heading">Modulos</div>
                                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                    Modulos
-                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    
+                                   
                                 </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="profesionalPrehospitalario.html">Profesional Prehospitalario</a>
-                                    <a class="nav-link" href="establecimientos.html">Establecimientos</a>
-                                </nav>
-                            </div>
+  
+                                    <a class="nav-link" href="profesionalPrehospitalario.php">Profesional Prehospitalario</a>
+                                    <a class="nav-link" href="establecimientos.php">Establecimientos</a>
+  
+         
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
